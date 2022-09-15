a2dc6af201f06a7993a9faa79eafd65fe63ffdd0
lihoiy <101048246+lihoiy@users.noreply.github.com>
2022-09-15 17:32:58 +0500

    First lesson Python

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+
+num = 3
+num2 = 5
+result = num + num2
+print(result)
+print('')
+num1 = num2 = 5
+print(num1, num2)
+print('')
+num_1, num_2 = 5, 7
+print(num_1, num_2)
+print('')
+
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 + swap2
+print(swap1, swap2)
+print('')
+
+swap2 -= num
+print(swap2)
+print('')
+
+z, x, c = [1, 2, 3]
+print(z)
+print(x)
+print(c)
+print('')
+
+*z, x, c = [1, 2, 3, 4, 5]
+print(z)
+print(x)
+print(c)
